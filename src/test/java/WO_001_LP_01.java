882eb9c163d75a271289cc5e1d96d168ef51e7b5
TahaFurkanC <cetinkayatahafurkan@gmail.com>
2023-12-31 11:22:39 +0100

    Working with weborder test case

---
+++ src/test/java/WO_001_LP_01.java
@@ -0,0 +1,8 @@
+public class WO_001_LP_01 {
+    // Open the URL.
+    // 2-) Click "WebOrder" button on top bar.
+    // 3-) Enter "Inar" as username and "Academy" password.
+    // 4-) Click on the "Login" button.
+    // 5-) Verify that the user is successfully logged in.
+
+}
